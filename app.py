259957066f7212f4e4540aa90be38da02253be9b
harsha-1707 <kattaharshav@gmail.com>
2026-07-27 12:27:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,4 +161,7 @@ with gr.Blocks(theme=theme, css=CUSTOM_CSS) as demo:
     )
 
 if __name__ == "__main__":
-    demo.queue().launch()
+    demo.queue().launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860)),
+    )
